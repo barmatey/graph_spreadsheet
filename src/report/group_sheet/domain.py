a267ea09fcd4d1e8f0d10a16b7c996e36cb1458b
barma <barmatey@gmail.com>
2023-09-25 15:19:29 +0300

    mapper

--- src/report/group_sheet/domain.py
+++ src/report/group_sheet/domain.py
@@ -3,20 +3,75 @@ from uuid import UUID, uuid4
 from pydantic import Field
 
 from src.core.cell import CellTable
+from src.core.pydantic_model import Model
 from src.node.domain import Command, Node, Event
-from src.report.wire.domain import Ccol
+from src.report.wire.domain import Ccol, WireNode
+from src.spreadsheet.sheet import domain as sheet_domain
 
 
-class SheetNode(Node):
+class PlanItems(Model):
+    ccols: list[Ccol]
+    uniques: dict[str, int] = Field(default_factory=dict)
+    value: CellTable = Field(default_factory=list)
+    uuid: UUID = Field(default_factory=uuid4)
+    events: list[Event] = Field(default_factory=list)
+
+
+class GroupSheetNode(sheet_domain.SheetNode):
+    plan_items: PlanItems
     uuid: UUID = Field(default_factory=uuid4)
     table: CellTable = Field(default_factory=list)
     events: list[Event] = Field(default_factory=list)
 
+    def __follow_wire(self, pub: WireNode):
+        row = [pub.__getattribute__(ccol) for ccol in self.plan_items.ccols]
+        key = str(row)
+        if self.plan_items.uniques.get(key) is None:
+            self.plan_items.value.append(row)
+            self.plan_items.uniques[key] = 1
+        else:
+            self.plan_items.uniques[key] += 1
+
+    def __update_wire(self, old_value: WireNode, new_value: WireNode):
+        old_row = [old_value.__getattribute__(ccol) for ccol in self.plan_items.ccols]
+        old_key = str(old_row)
+
+        new_row = [new_value.__getattribute__(ccol) for ccol in self.plan_items.ccols]
+        new_key = str(new_row)
+
+        # Drop old value
+        utable = self.plan_items.value
+        self.plan_items.uniques[old_key] -= 1
+        if self.plan_items.uniques[old_key] == 0:
+            del self.plan_items.uniques[old_key]
+            for i, row in enumerate(utable):
+                if str(row) == old_key:
+                    del utable[i]
+
+        if self.plan_items.uniques.get(new_key) is not None:
+            self.plan_items.uniques[new_key] += 1
+            return
+        self.plan_items.uniques[new_key] = 1
+        utable.append(new_row)
+
+        self._on_updated()
+        return
+
     def follow(self, pubs: set['Node']):
-        raise NotImplemented
+        for pub in pubs:
+            if isinstance(pub, WireNode):
+                self.__follow_wire(pub)
+            else:
+                raise TypeError(f"invalid type: {type(pub)}")
+
+        self._on_updated()
+        self._on_subscribed(pubs)
 
     def update(self, old_value: 'Node', new_value: 'Node'):
-        raise NotImplemented
+        if isinstance(old_value, WireNode) and isinstance(new_value, WireNode):
+            self.__update_wire(old_value, new_value)
+        else:
+            raise TypeError(f"Expected type is SourceNode but real type is {type(new_value)}")
 
 
 class CreateGroupSheetNode(Command):
--- src/report/group_sheet/handlers.py
+++ src/report/group_sheet/handlers.py
@@ -5,16 +5,16 @@ from src.report.wire import domain as wire_domain
 from src.report.formula.utable import domain as utable_domain
 from src.report.formula.mapper import domain as mapper_domain
 from src.spreadsheet.cell import domain as cell_domain
-from . import domain as sheet_domain
+from . import domain as group_sheet_domain
 
 
 class GetSheetByIdHandler(CommandHandler):
-    def execute(self, cmd: sheet_domain.GetSheetById) -> sheet_domain.SheetNode:
+    def execute(self, cmd: group_sheet_domain.GetSheetById) -> group_sheet_domain.GroupSheetNode:
         raise NotImplemented
 
 
 class CreateGroupSheetNodeHandler(CommandHandler):
-    def execute(self, cmd: sheet_domain.CreateGroupSheetNode) -> sheet_domain.SheetNode:
+    def execute(self, cmd: group_sheet_domain.CreateGroupSheetNode) -> group_sheet_domain.GroupSheetNode:
         logger.error("CreateGroup.execute()")
 
         # Get wires
@@ -22,33 +22,31 @@ class CreateGroupSheetNodeHandler(CommandHandler):
         wires = set(filter(lambda x: isinstance(x, wire_domain.WireNode), self._repo.get_node_parents(source)))
 
         # Create
-        utable = utable_domain.UtableNode(ccols=cmd.ccols)
-        self._repo.add(utable)
-        utable.follow(wires)
-        self.extend_events(utable.parse_events())
-
-        group = sheet_domain.SheetNode()
+        group = group_sheet_domain.GroupSheetNode(plan_items=group_sheet_domain.PlanItems(ccols=cmd.ccols))
         self._repo.add(group)
 
-        for i in range(0, len(utable.value)):
-            row = []
-            mapper = mapper_domain.MapperNode(ccols=cmd.ccols)
-            self._repo.add(mapper)
-            for j in range(0, len(utable.value[0])):
-                cell = cell_domain.CellNode(index=(i, j), value=utable.value[i][j])
-                self._repo.add(cell)
-                cell.follow({utable})
-                row.append(cell.value)
-                self.extend_events(cell.parse_events())
-                mapper.follow({cell})
-            logger.success(mapper.filter_by)
-            self.extend_events(mapper.parse_events())
-            group.table.append(row)
+        group.follow(wires)
+        self.extend_events(group.parse_events())
+
+        # for i in range(0, len(utable.value)):
+        #     row = []
+        #     mapper = mapper_domain.MapperNode(ccols=cmd.ccols)
+        #     self._repo.add(mapper)
+        #     for j in range(0, len(utable.value[0])):
+        #         cell = cell_domain.CellNode(index=(i, j), value=utable.value[i][j])
+        #         self._repo.add(cell)
+        #         cell.follow({utable})
+        #         row.append(cell.value)
+        #         self.extend_events(cell.parse_events())
+        #         mapper.follow({cell})
+        #     logger.success(mapper.filter_by)
+        #     self.extend_events(mapper.parse_events())
+        #     group.table.append(row)
 
         return group
 
 
 GROUP_COMMAND_HANDLERS = {
-    sheet_domain.CreateGroupSheetNode: CreateGroupSheetNodeHandler,
-    sheet_domain.GetSheetById: GetSheetByIdHandler,
+    group_sheet_domain.CreateGroupSheetNode: CreateGroupSheetNodeHandler,
+    group_sheet_domain.GetSheetById: GetSheetByIdHandler,
 }
