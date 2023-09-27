59c697aa4878bd343fdff5c138c4bdce570e3c3a
barma <barmatey@gmail.com>
2023-09-27 17:12:46 +0300

    common

---
+++ tests/report/before.py
@@ -0,0 +1,37 @@
+from uuid import uuid4
+
+from src.messagebus.msgbus import Msgbus
+from src.report.source import domain as source_domain
+from src.report.wire import domain as wire_domain
+
+
+def execute(cmd):
+    bus = Msgbus()
+    bus.push_command(cmd)
+    bus.run()
+    result = bus.results[cmd.uuid]
+    return result
+
+
+def load_data():
+    # Source
+    cmd_source = source_domain.CreateSourceNode(uuid=source_uuid, title="Hello")
+    source = execute(cmd_source)
+
+    # Wire
+    cmd_wire1 = wire_domain.CreateWireNode(uuid=wire1_uuid, sender=1, receiver=11, amount=1, sub1="Hello",
+                                           source_id=source.uuid)
+    cmd_wire2 = wire_domain.CreateWireNode(uuid=wire2_uuid, sender=2, receiver=22, amount=3, sub1="World",
+                                           source_id=source.uuid)
+    cmd_wire3 = wire_domain.CreateWireNode(uuid=wire3_uuid, sender=3, receiver=33, amount=12, sub1="Anna!",
+                                           source_id=source.uuid)
+    execute(cmd_wire1)
+    execute(cmd_wire2)
+    execute(cmd_wire3)
+
+
+source_uuid = uuid4()
+wire1_uuid = uuid4()
+wire2_uuid = uuid4()
+wire3_uuid = uuid4()
+group_sheet_uuid = uuid4()
--- tests/report/test_group_sheet.py
+++ tests/report/test_group_sheet.py
@@ -1,42 +1,9 @@
-from uuid import uuid4
 import pytest
 
-from src.messagebus.msgbus import Msgbus
 from src.node.repository import GraphRepoFake
-from src.report.source import domain as source_domain
 from src.report.wire import domain as wire_domain
 from src.report.group_sheet import domain as group_domain
-
-source_uuid = uuid4()
-wire1_uuid = uuid4()
-wire2_uuid = uuid4()
-wire3_uuid = uuid4()
-group_sheet_uuid = uuid4()
-
-
-def execute(cmd):
-    bus = Msgbus()
-    bus.push_command(cmd)
-    bus.run()
-    result = bus.results[cmd.uuid]
-    return result
-
-
-def load_data():
-    # Source
-    cmd_source = source_domain.CreateSourceNode(uuid=source_uuid, title="Hello")
-    source = execute(cmd_source)
-
-    # Wire
-    cmd_wire1 = wire_domain.CreateWireNode(uuid=wire1_uuid, sender=1, receiver=11, amount=1, sub1="Hello",
-                                           source_id=source.uuid)
-    cmd_wire2 = wire_domain.CreateWireNode(uuid=wire2_uuid, sender=2, receiver=22, amount=3, sub1="World",
-                                           source_id=source.uuid)
-    cmd_wire3 = wire_domain.CreateWireNode(uuid=wire3_uuid, sender=3, receiver=33, amount=12, sub1="Anna!",
-                                           source_id=source.uuid)
-    execute(cmd_wire1)
-    execute(cmd_wire2)
-    execute(cmd_wire3)
+from tests.report.before import execute, load_data, source_uuid, wire1_uuid, wire2_uuid, wire3_uuid, group_sheet_uuid
 
 
 @pytest.fixture(scope="module")
