e87ed7f57316d52b8d758773bb2597e4d15b1d9b
barma <barmatey@gmail.com>
2023-09-27 10:53:12 +0300

    events

--- src/node/domain.py
+++ src/node/domain.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from collections import OrderedDict
 from uuid import UUID, uuid4
 from pydantic import Field
 
@@ -15,7 +16,7 @@ class Event(Model):
 
 class Node(Model):
     uuid: UUID
-    events: list[Event]
+    events: 'EventQueue'
 
     def __repr__(self):
         return self.__class__.__name__
@@ -36,7 +37,7 @@ class Node(Model):
         self._on_subscribed(pubs)
 
     def _on_updated(self):
-        self.events.append(NodeUpdated(old_value=self.model_copy(deep=True), new_value=self))
+        self.events.append_node_updated_event(NodeUpdated(old_value=self.model_copy(deep=True), new_value=self))
 
     def _on_subscribed(self, pubs: set['Node']):
         self.events.append(NodeSubscribed(sub=self, pubs=pubs))
@@ -48,9 +49,7 @@ class Node(Model):
         self.events.append(NodeUpdated(old_value=old_value, new_value=self))
 
     def parse_events(self) -> list[Event]:
-        events = self.events
-        self.events = []
-        return events
+        return self.events.parse_events()
 
 
 class NodeUpdated(Event):
@@ -63,3 +62,24 @@ class NodeSubscribed(Event):
     pubs: set[Node]
     sub: Node
     uuid: UUID = Field(default_factory=uuid4)
+
+
+class EventQueue:
+    def __init__(self):
+        self._events = OrderedDict()
+
+    def append_node_updated_event(self, event: NodeUpdated):
+        key = type(event)
+        if self._events.get(key) is None:
+            self._events[key] = event
+        else:
+            old = self._events.pop(key)
+            self._events[key] = NodeUpdated(old_value=old.old_value, new_value=event.new_value)
+
+    def append(self, event: Event):
+        self._events[event.uuid] = event
+
+    def parse_events(self) -> list[Event]:
+        events = self._events
+        self._events = OrderedDict()
+        return list(events.values())
